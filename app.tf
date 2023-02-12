0a742eb88471cae18cf2600a9f2ffd17adaa71ba
Jeff Spradlin <jeff.spradlin@slalom.com>
2023-02-11 22:38:51 -0600

    removing always on

--- app.tf
+++ app.tf
@@ -18,6 +18,8 @@ resource "azurerm_linux_web_app" "linux" {
   resource_group_name = azurerm_resource_group.rg01.name
   location            = azurerm_resource_group.rg01.location
   service_plan_id     = azurerm_service_plan.asp.id
-  always_on           = false
-  site_config {}
+  
+  site_config {
+    always_on           = false
+  }
 }
