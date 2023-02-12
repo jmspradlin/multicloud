1991894669dbebf0f58105b4011df1f2bf32ae80
Jeff Spradlin <jeff.spradlin@slalom.com>
2023-02-11 21:12:06 -0600

    updating resource names

--- azure/main.tf
+++ azure/main.tf
@@ -42,9 +42,9 @@ resource "azurerm_windows_web_app" "windows" {
   for_each = var.app_service
 
   name                = each.value.name
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_service_plan.example.location
-  service_plan_id     = azurerm_service_plan.example.id
+  resource_group_name = azurerm_resource_group.rg01.name
+  location            = azurerm_service_plan.windows.location
+  service_plan_id     = azurerm_service_plan.windows.id
 
   site_config {}
 }
