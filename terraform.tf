2e6436b51567a0dc9c2506d83ba501d2cb99d3b0
Jeff Spradlin <jeff.spradlin@slalom.com>
2023-02-11 19:42:19 -0600

    adding terraform cloud configuration

---
+++ terraform.tf
@@ -0,0 +1,22 @@
+terraform {
+    cloud {
+        organization = "loudtree"
+
+        workspaces {
+            name = "multicloud"
+        }
+    }
+
+    required_providers {
+        aws = {
+            source = "hashicorp/aws"
+            version = "4.54.0"
+        }
+        azurerm = {
+            source  = "hashicorp/azurerm"
+            version = "~> 3.43.0"
+        }
+    }
+    
+  required_version = ">= 1.1.0"
+}
