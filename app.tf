58c950da4dd2ec4568b9f5a8cbd4e7c1c62faee2
Jeff Spradlin <jeff.spradlin@slalom.com>
2023-02-11 22:33:23 -0600

    updating rg reference

--- app.tf
+++ app.tf
@@ -6,8 +6,8 @@ resource "azurerm_resource_group" "rg01" {
 resource "azurerm_service_plan" "asp" {
   name                = "${var.env}-${var.app_service_plan.name}"
   sku_name            = var.app_service_plan.sku_name
-  resource_group_name = var.rg.name
-  location            = var.rg.location
+  resource_group_name = azurerm_resource_group.rg01.name
+  location            = azurerm_resource_group.rg01.location
   os_type             = var.app_service_plan.os
 }
 
@@ -15,8 +15,8 @@ resource "azurerm_linux_web_app" "linux" {
   for_each = var.app_service_linux
 
   name                = "${var.env}-${each.key}"
-  resource_group_name = var.rg.name
-  location            = var.rg.location
+  resource_group_name = azurerm_resource_group.rg01.name
+  location            = azurerm_resource_group.rg01.location
   service_plan_id     = azurerm_service_plan.asp.id
 
   site_config {}
