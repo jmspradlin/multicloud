1551204f1d1187593f457013220ad4b4e533ad42
Jeff Spradlin <jeff.spradlin@slalom.com>
2023-02-19 20:58:09 -0600

    adding source address prefix for public rule

--- azure.tf
+++ azure.tf
@@ -46,6 +46,7 @@ resource "azurerm_network_security_rule" "public" {
   protocol                    = each.value.protocol
   source_port_range           = each.value.source_port_range
   destination_port_range      = each.value.destination_port_range
+  source_address_prefix       = each.value.source_address_prefix
   destination_address_prefix  = each.value.destination_address_prefix
   resource_group_name         = azurerm_resource_group.rg01.name
   network_security_group_name = azurerm_network_security_group.public.name
