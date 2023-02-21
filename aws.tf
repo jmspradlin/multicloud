d2e1556f2908ea6db6b739a63b6c0f041d965bac
Jeff Spradlin <jeff.spradlin@slalom.com>
2023-02-21 12:25:38 -0600

    rolling up dependencies

--- aws.tf
+++ aws.tf
@@ -61,14 +61,14 @@ module "aws_instance01" {
   vpc_security_group_ids = [aws_security_group.sg["private"].id]
   tags                   = var.tags
   depends_on             = [
-    module.vpc,
-    aws_security_group.sg["private"],
-    aws_security_group.sg["public"],
-    aws_security_group_rule.public["ingress1"],
-    aws_security_group_rule.public["egress1"],
-    aws_security_group_rule.private["ingress80"],
-    aws_security_group_rule.private["ingress443"],
-    aws_security_group_rule.private["egress1"]
+    module.vpc
+    # aws_security_group.sg["private"],
+    # aws_security_group.sg["public"],
+    # aws_security_group_rule.public["ingress1"],
+    # aws_security_group_rule.public["egress1"],
+    # aws_security_group_rule.private["ingress80"],
+    # aws_security_group_rule.private["ingress443"],
+    # aws_security_group_rule.private["egress1"]
 
   ]
   user_data              = <<-EOF
